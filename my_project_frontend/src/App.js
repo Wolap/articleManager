17a0242d302c6f99b08735df6d3b8dd0e5024d4d
Wolap <wolap.wolap@gmail.com>
2024-12-15 23:07:57 +0100

    display articles

--- my_project_frontend/src/App.js
+++ my_project_frontend/src/App.js
@@ -1,34 +1,86 @@
-import React, { useState } from 'react';
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import { TextField, Button, Container, Typography } from "@mui/material";
 
-const App = () => {
-  const [article, setArticle] = useState({
-    topic: '',
-    author: '',
-    body: '',
-  });
+function App() {
+  const [articles, setArticles] = useState([]); 
+  const [form, setForm] = useState({ topic: "", author: "", body: "" }); 
+  const [editMode, setEditMode] = useState(false); 
+  const [selectedArticleId, setSelectedArticleId] = useState(null); 
+
+  useEffect(() => {
+    fetch("http://localhost:8000/api/articles/")
+      .then((res) => res.json())
+      .then((data) => setArticles(data));
+  }, []);
 
   const handleChange = (e) => {
-      setArticle({ ...article, [e.target.name]: e.target.value });
+    setForm({ ...form, [e.target.name]: e.target.value });
   };
 
-  const handleSubmit = async () => {
-    try {
-      const response = await axios.post('http://localhost:8000/api/articles/', article);
-      console.log(response.data);
-    } catch (error) {
-      console.error(error);
-    }
+  const handleAddArticle = () => {
+    fetch("http://localhost:8000/api/articles/", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(form),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setArticles([...articles, data]); 
+        setForm({ topic: "", author: "", body: "" });
+      });
+  };
+
+  const handleEdit = (article) => {
+    setForm({ topic: article.topic, author: article.author, body: article.body });
+    setEditMode(true);
+    setSelectedArticleId(article.id); 
+  };
+
+  const handleUpdateArticle = () => {
+    fetch(`http://localhost:8000/api/articles/${selectedArticleId}/`, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(form),
+    })
+      .then((res) => res.json())
+      .then((updatedArticle) => {
+
+        const updatedArticles = articles.map((article) =>
+          article.id === updatedArticle.id ? updatedArticle : article
+        );
+        setArticles(updatedArticles);
+        setForm({ topic: "", author: "", body: "" });
+        setEditMode(false);
+        setSelectedArticleId(null);
+      });
   };
 
   return (
-    <div style={{ margin: '20px' }}>
+    <Container>
+      <Typography variant="h4" component="h1" gutterBottom>
+        Articles
+      </Typography>
+
+      {articles.map((article) => (
+        <div key={article.id} style={{ marginBottom: "20px", padding: "10px", border: "1px solid #ddd" }}>
+          <Typography variant="h6">{article.topic}</Typography>
+          <Typography>{article.body}</Typography>
+          <Typography variant="caption">Author: {article.author}</Typography>
+          <div style={{ marginTop: "10px" }}>
+            <Button variant="outlined" color="primary" onClick={() => handleEdit(article)}>
+              Edit
+            </Button>
+          </div>
+        </div>
+      ))}
+
+      <Typography variant="h5" component="h2" gutterBottom>
+        {editMode ? "Edit Article" : "Add Article"}
+      </Typography>
       <TextField
         label="Topic"
         name="topic"
-        value={article.topic}
+        value={form.topic}
         onChange={handleChange}
         fullWidth
         margin="normal"
@@ -36,7 +88,7 @@ const App = () => {
       <TextField
         label="Author"
         name="author"
-        value={article.author}
+        value={form.author}
         onChange={handleChange}
         fullWidth
         margin="normal"
@@ -44,18 +96,23 @@ const App = () => {
       <TextField
         label="Body"
         name="body"
-        value={article.body}
+        value={form.body}
         onChange={handleChange}
         fullWidth
         multiline
         rows={4}
         margin="normal"
       />
-      <Button variant="contained" color="primary" onClick={handleSubmit}>
-        Submit
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={editMode ? handleUpdateArticle : handleAddArticle}
+        style={{ marginTop: "10px" }}
+      >
+        {editMode ? "Save Changes" : "Add Article"}
       </Button>
-    </div>
+    </Container>
   );
-};
+}
 
 export default App;
